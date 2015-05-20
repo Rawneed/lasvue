a62c2c933fa309dd16f63112573cfeca6fe76a42
Bode Fuwa <bodefuwa@gmail.com>
2015-05-21 00:36:34 +0100

    Added page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.	
+  def full_title(page_title = '')
+  	default_title = "Lasvue: View Lagos State"
+  	if page_name.empty? 
+  	  default_title
+  	else 
+  	  default_title + "| #{page_title}"
+  	end
+  end
 end
