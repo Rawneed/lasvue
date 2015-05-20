ad8d9ce45393819810431ca57c5ce8bfabb5f0bc
Bode Fuwa <bodefuwa@gmail.com>
2015-05-21 00:26:35 +0100

    Added seed data structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,66 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# models of Lasvue
+#  local_government, 
+#   symbol: symbol_name: , meaning:
+# 	population_trend: year:, population:, global_ranking:
+# 	National Assembly: person_name:, region/district:
+# 	Members of Executive Council: name: title: contact:
+#  	House of Assembly: name:, title:, region/disctrict:
+# 	Body of Benchers[Judges], Chief Judge
+# 	Magistrates [Chief Registrar]
+# 	Body of Permanent Secretaries
+# 	Director_Generals
+# 	Members of LG Civil Service Commission: name:, title:, division:
+#  	Members of Loval Gov Service Commission:
+#   Members of Judicial Service Commission
+# 	Members of State Independent Electoral Commission
+#  	Members of Health Service Commission
+# 	Lagos Sate Councile of Obas and Chiefs
+#  	Ministries, Offices, and Bureaux
+# 	Extra-Ministerial Depts/Commissions
+# 	LS Parastatals/Agencies
+# 	Police Commands
+#   Fire Stations
+#  	Lagos Internal Revenue Service Tax Offices
+# 	Lagos State Drivers Institutes
+# 	Drivers Licence Centres
+# 	MVA
+# 	LS Census Population Figures
+# 	Emergency/Vital Telephone Numbers
+# 	Docs Required For Regularization of Land
+# 	Chairmen of 20 Local Gov. & 37 Local Council Dev Areas
+# 	Tertiary Institurtions
+
+# models
+#  local_government, 
+#   symbol: symbol_name: , meaning:
+# 	population_trend: year:, population:, global_ranking:
+# 	National Assembly: person_name:, region/district:
+# 	Members of Executive Council: name: title: contact:
+#  	House of Assembly: name:, title:, region/disctrict:
+# 	Body of Benchers[Judges], Chief Judge
+# 	Magistrates [Chief Registrar]
+# 	Body of Permanent Secretaries
+# 	Director_Generals
+# 	Members of LG Civil Service Commission: name:, title:, division:
+#  	Members of Loval Gov Service Commission:
+#   Members of Judicial Service Commission
+# 	Members of State Independent Electoral Commission
+#  	Members of Health Service Commission
+# 	Lagos Sate Councile of Obas and Chiefs
+#  	Ministries, Offices, and Bureaux
+# 	Extra-Ministerial Depts/Commissions
+# 	LS Parastatals/Agencies
+# 	Police Commands
+#   Fire Stations
+#  	Lagos Internal Revenue Service Tax Offices
+# 	Lagos State Drivers Institutes
+# 	Drivers Licence Centres
+# 	MVA
+# 	LS Census Population Figures
+# 	Emergency/Vital Telephone Numbers
+# 	Docs Required For Regularization of Land
+# 	Chairmen of 20 Local Gov. & 37 Local Council Dev Areas
+# 	Tertiary Institurtions
