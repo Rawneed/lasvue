851a06a1b6ea21359aa5f09fc7b4e57d19374398
Bode Fuwa <bodefuwa@gmail.com>
2015-05-24 03:20:42 +0100

    Displaying Divisions and LGAs

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,5 +1,10 @@
 class RegionsController < ApplicationController
+  
+
   def divisions
+  	@divisions = Division.all
+  	@division = Division.find_by(params[:name])
+  	@lgas = @division.local_areas
   end
 
   def local_government_areas
